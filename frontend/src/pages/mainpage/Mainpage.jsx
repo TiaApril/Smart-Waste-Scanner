a1821401338c46052b45e89b253362e3805a97ef
MARYANNE67 <93769788+MARYANNE67@users.noreply.github.com>
2023-08-19 18:09:35 -0400

    Changed layout of pages and made scanner more reponsive

--- frontend/src/pages/mainpage/Mainpage.jsx
+++ frontend/src/pages/mainpage/Mainpage.jsx
@@ -1,24 +1,28 @@
-import React, { useState } from 'react'
-import CameraScanner from '../../component/camerascanner/CameraScanner'
-import './mainpage.css'
+import React, { useState } from 'react';
+import CameraScanner from '../../component/camerascanner/CameraScanner';
+import './mainpage.css';
 import Navbar from '../../component/navbar/Navbar';
 import FootNavBar from '../../component/footbar/FootNavBar';
 import UploadPicture from '../../component/uploadpicture/UploadPicture';
 
 function Mainpage() {
-    const [scannedImage, setScannedImage] = useState(null);
+  const [scannedImage, setScannedImage] = useState(null);
 
   const handleScan = (imageDataUrl) => {
     setScannedImage(imageDataUrl);
-  }
+  };
+
   return (
-    <div className='mainpage'>
-        <Navbar/>
-        <CameraScanner onScan={handleScan}/>
-        <UploadPicture/>
-        <FootNavBar/>
+    <div className='mainpage-container'>
+      <Navbar />
+      <div className='mainpage-content'>
+        <CameraScanner onScan={handleScan} />
+        <UploadPicture />
+      </div>
+      <FootNavBar />
     </div>
-  )
+  );
 }
 
-export default Mainpage
+export default Mainpage;
+
